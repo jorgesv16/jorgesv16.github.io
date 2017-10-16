38c19baf0ceeeefdb40d7db47d0a984bddbd00de
Jorge Salas <jorge_salas@Jorges-MacBook-Pro.local>
2017-10-16 10:05:21 -0700

    crystals game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,53 @@
-var words = ["cairo", "dakar", "lagos", "paris", "quito", "seoul", "tokyo", "milan", "dhaka", "kyoto"];
+var target;
+var counter = 0;
+var winCounter = 0;
+var lossCounter = 0;
 
-var guesses = [];
-var winsCounter = document.getElementById("wins");
-var remainingCounter = 10;
-var wordsPlayed = [];
-var userGuess;
-var gameCounter = 0;
-var letterCounter = 0;
-var wordPicked;
-
-var pickWord = function() {
-	var wordPick;
-	var arr;
-
-	do {
-		wordPick = words[(Math.floor((Math.random() * 10) + 1))];
-	} while (wordsPlayed.indexOf(wordPick) >= 0);
-
-	wordsPlayed[gameCounter] = wordPick;
-	arr = wordPick.split("");
-
-	return arr;
+function getRandomInt(min, max) {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
+function resetValues() {
 
+    $("#red").attr("data-crystalvalue", getRandomInt(1, 12));
+    $("#blue").attr("data-crystalvalue", getRandomInt(1, 12));
+    $("#yellow").attr("data-crystalvalue", getRandomInt(1, 12));
+    $("#green").attr("data-crystalvalue", getRandomInt(1, 12));
 
-var reseter = function() {
-	remainingCounter = 10;
-	guesses = [];
-	letterCounter = 0;
+    target = getRandomInt(19, 120);
+    counter = 0;
+    $("#current-score").text(counter);
+    $("#target-score").text(target);
 }
 
-var printCorrectGuesses = function(guesses) {
-
+function youWon() {
+	winCounter++;
+	$("#wins").text(winCounter);
 }
 
-var printIncorrectGuesses = function(guesses){
-
+function youLost() {
+	lossCounter++;
+	$("#losses").text(lossCounter);
 }
 
-wordPicked = pickWord();
-document.onkeyup = function(event) {
+resetValues();
 
-	userGuess = event.key;
-	if (guesses.indexOf(userGuess) < 0) {
-		guesses[letterCounter] = userGuess;
-		letterCounter++;
-	}
+$(".crystal-image").on("click", function() {
+	var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
 
-	console.log(wordPicked);
+    counter += crystalValue;
+    $("#current-score").text(counter);
+    
+    // Determine if win/loss, and reset values
+    if (counter >= target) {
 
-	// reseter();
+    	if (counter === target) {
+    		youWon();
+    	} else {
+    		youLost();
+    	}
 
-	
-	// console.log(guesses);
-	// console.log(guesses);
-
-}
+    	resetValues();
+    }
+});
