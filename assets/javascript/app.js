fceec9983c426e603fc72895c20b27eacbbb8c95
Jorge Salas <jorge_salas@Jorges-MacBook-Pro.local>
2017-10-20 10:26:40 -0700

    trivia game update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -103,7 +103,6 @@ function displayCorrectAnswer (i, event) {
 	clearInterval(counting);
 	count = 0;
 
-	
 	if (event === undefined) {
 
 		$(".second-row").empty();
@@ -141,54 +140,4 @@ function displayCorrectAnswer (i, event) {
 };
 
 trivia.displayStart();
-
-
-
-
-		// function renderButtons() {
-
-  //       // Deletes the movies prior to adding new movies
-  //       // (this is necessary otherwise you will have repeat buttons)
-  //       $("#buttons-view").empty();
-
-  //       // Loops through the array of movies
-  //       for (var i = 0; i < movies.length; i++) {
-
-  //         // Then dynamicaly generates buttons for each movie in the array
-  //         // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-  //         var a = $("<button>");
-  //         // Adds a class of movie to our button
-  //         a.addClass("movie");
-  //         // Added a data-attribute
-  //         a.attr("data-name", movies[i]);
-  //         // Provided the initial button text
-  //         a.text(movies[i]);
-  //         // Added the button to the buttons-view div
-  //         $("#buttons-view").append(a);
-  //       }
-  //     }
-
-  //     // This function handles events where the add movie button is clicked
-  //     $("#add-movie").on("click", function(event) {
-  //       event.preventDefault();
-  //       // This line of code will grab the input from the textbox
-  //       var movie = $("#movie-input").val().trim();
-  //       movie = movie.charAt(0).toUpperCase() + movie.slice(1);
-
-  //       // The movie from the textbox is then added to our array
-  //       movies.push(movie);
-
-  //       // Calling renderButtons which handles the processing of our movie array
-  //       renderButtons();
-
-  //     });
-
-  //     // Adding click event listeners to all elements with a class of "movie"
-  //     $(document).on("click", ".movie", displayMovieInfo);
-
-  //     // Calling the renderButtons function to display the intial buttons
-  //     renderButtons();
-
-
-
 
